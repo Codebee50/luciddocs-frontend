2db2c13677cd70f783f34c1d4e6346f41efbe4a8
codebee50 <codebee345@outlook.com>
2025-11-13 11:53:10 -0500

    changes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -47,7 +47,7 @@ const Nav = () => {
       </div>
 
       <Link
-        href="/login"
+        href="/waitlist"
         className="max-sm:hidden rounded-lg px-7 py-2 overflow-hidden group bg-green-700 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300"
       >
         <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
